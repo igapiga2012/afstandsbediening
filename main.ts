17f3c9e0aa8d241652806e64c003b23384b8a4a4
igapiga2012 <igawarzych@gmail.com>
2025-12-02 13:26:28 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,9 +1,15 @@
 let IR_waarde = 0
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 IR.IR_init()
 let vooruit = 47
 let achteruit = 71
 let links = 15
 let rechts = 13
+let stop = 6
+basic.forever(function () {
+    IR_waarde = IR.IR_read()
+    strip.showColor(neopixel.colors(NeoPixelColors.Purple))
+})
 basic.forever(function () {
     if (IR_waarde == vooruit) {
         basic.showLeds(`
@@ -14,8 +20,6 @@ basic.forever(function () {
             . . # . .
             `)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 90)
-        basic.pause(1000)
-        maqueen.motorStop(maqueen.Motors.All)
     }
     if (IR_waarde == achteruit) {
         basic.showLeds(`
@@ -26,8 +30,6 @@ basic.forever(function () {
             . . # . .
             `)
         maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 90)
-        basic.pause(1000)
-        maqueen.motorStop(maqueen.Motors.All)
     }
     if (IR_waarde == links) {
         basic.showLeds(`
@@ -39,8 +41,6 @@ basic.forever(function () {
             `)
         maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 90)
-        basic.pause(850)
-        maqueen.motorStop(maqueen.Motors.M1)
     }
     if (IR_waarde == rechts) {
         basic.showLeds(`
@@ -51,20 +51,16 @@ basic.forever(function () {
             . . # . .
             `)
         maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 90)
-        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 90)
-        basic.pause(850)
-        maqueen.motorStop(maqueen.Motors.M2)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 90)
     }
-    basic.showLeds(`
-        . . . . .
-        . # # # .
-        . # . # .
-        . # # # .
-        . . . . .
-        `)
-    maqueen.motorStop(maqueen.Motors.All)
-    basic.pause(1000)
-})
-basic.forever(function () {
-    IR_waarde = IR.IR_read()
+    if (IR_waarde == stop) {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # . # .
+            . # # # .
+            . . . . .
+            `)
+        maqueen.motorStop(maqueen.Motors.All)
+    }
 })
